133448fd73a0f29aec8efe89d9d18f5b369d4f69
sungmin7465 <sungmin7465@gmail.com>
2017-12-01 13:57:10 +0900

    Added url-loader configure

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,18 @@ module.exports = {
         {
             test: /\.svg$/,
             loader: 'svg-url-loader?noquotes'
-        }]
+        },
+	{
+            test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader',
+            options: {
+                name: '[hash].[ext]',
+                limit: 10000
+            }
+	}
+	
+	
+	]
     },
     plugins: [
         new webpack.DefinePlugin({
